b7ed45d01433a0233f3dcb7ee604a7b608e332e4
Watson <zeroneho@163.com>
2024-07-11 09:07:40 +0800

    增加注释

--- src/main/java/com/zzjee/wm/controller/WvStockSttBinController.java
+++ src/main/java/com/zzjee/wm/controller/WvStockSttBinController.java
@@ -435,6 +435,7 @@ public class WvStockSttBinController extends BaseController {
 		return new ResponseEntity(headers, HttpStatus.CREATED);
 	}
 
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> update(@RequestBody WvStockSttBinEntity wvStockSttBin) {
 		//调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
