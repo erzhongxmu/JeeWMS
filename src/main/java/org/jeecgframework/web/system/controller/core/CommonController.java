c7cf0641a28ad3fdd590550dc6dea63f0814608c
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2024-10-09 15:44:28 +0800

    文件上传漏洞

--- src/main/java/org/jeecgframework/web/system/controller/core/CommonController.java
+++ src/main/java/org/jeecgframework/web/system/controller/core/CommonController.java
@@ -191,7 +191,7 @@ public class CommonController extends BaseController {
 				systemService.parserXml(savePath); // 使用完整路径
 				json.setSuccess(true);
 			} catch (IOException e) {
-				e.printStackTrace(); // 最好使用日志记录器记录异常
+				e.printStackTrace(); 
 				json.setMsg("Failed to save file.");
 				json.setSuccess(false);
 			}
