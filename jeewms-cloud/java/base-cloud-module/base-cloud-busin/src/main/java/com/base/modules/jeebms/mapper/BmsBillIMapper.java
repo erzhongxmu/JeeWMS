60a29e2f6d4f84e3797281318be8de9652d464af
Yu <yinyu1224yu@163.com>
2025-05-20 09:20:11 +0800

    阿里巴巴代码规范,所有的抽象方法（包括接口中的方法）必须要用javadoc注释、除了返回值、参数、异常说明外，还必须指出该方法做什么事情，实现什么功能。

--- jeewms-cloud/java/base-cloud-module/base-cloud-busin/src/main/java/com/base/modules/jeebms/mapper/BmsBillIMapper.java
+++ jeewms-cloud/java/base-cloud-module/base-cloud-busin/src/main/java/com/base/modules/jeebms/mapper/BmsBillIMapper.java
@@ -13,7 +13,17 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @Version: V1.0
  */
 public interface BmsBillIMapper extends BaseMapper<BmsBillI> {
+    /**
+     * 批量删除
+     * @param mainId
+     * @return
+     */
     public boolean deleteByMainId(@Param("mainId") String mainId);
 
+    /**
+     * 查询
+     * @param mainId
+     * @return
+     */
     public List<BmsBillI> selectByMainId(@Param("mainId") String mainId);
 }
