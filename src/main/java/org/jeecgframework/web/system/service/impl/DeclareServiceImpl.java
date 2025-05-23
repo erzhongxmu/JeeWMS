3d04bc61855b3ecfaa41edd162f2363c8d461186
LongLiu <2074413691@qq.com>
2025-05-23 18:17:56 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/web/system/service/impl/DeclareServiceImpl.java
+++ src/main/java/org/jeecgframework/web/system/service/impl/DeclareServiceImpl.java
@@ -9,16 +9,20 @@ import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("declareService")
 @Transactional
 public class DeclareServiceImpl extends CommonServiceImpl implements DeclareService {
 
-	@Override
-    public List<TSAttachment> getAttachmentsByCode(String businessKey, String description)
-	{
-		String hql="from TSAttachment t where t.businessKey='"+businessKey+"' and t.description='"+description+"'";
-		return commonDao.findByQueryString(hql);
-	}
-	
+    @Override
+    public List<TSAttachment> getAttachmentsByCode(String businessKey, String description) {
+        String hql = "from TSAttachment t where t.businessKey='" + businessKey + "' and t.description='" + description + "'";
+        return commonDao.findByQueryString(hql);
+    }
+
 }
