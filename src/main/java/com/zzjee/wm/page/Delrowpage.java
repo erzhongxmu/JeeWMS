b1dbe50bc187729d4d6f21a2e8c1317da47ca3f4
LongLiu <2074413691@qq.com>
2025-05-27 11:23:18 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/page/Delrowpage.java
+++ src/main/java/com/zzjee/wm/page/Delrowpage.java
@@ -5,8 +5,11 @@ import java.util.List;
 import com.zzjee.wm.entity.WmToDownGoodsEntity;
 
 
-
 /**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
  */
 public class Delrowpage {
     private List<WmToDownGoodsEntity> downrows;
