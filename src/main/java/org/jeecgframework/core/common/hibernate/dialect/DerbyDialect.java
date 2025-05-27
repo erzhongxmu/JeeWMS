1712f64a5b14d015b8b315bae13409b9d1b8cc4f
LongLiu <2074413691@qq.com>
2025-05-27 11:23:49 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/core/common/hibernate/dialect/DerbyDialect.java
+++ src/main/java/org/jeecgframework/core/common/hibernate/dialect/DerbyDialect.java
@@ -1,23 +1,24 @@
 package org.jeecgframework.core.common.hibernate.dialect;
 
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
+public class DerbyDialect extends Dialect {
+    @Override
+    public boolean supportsLimit() {
+        return false;
+    }
 
-public class DerbyDialect extends Dialect
-{
-  @Override
-  public boolean supportsLimit()
-  {
-    return false;
-  }
+    @Override
+    public boolean supportsLimitOffset() {
+        return false;
+    }
 
-  @Override
-  public boolean supportsLimitOffset()
-  {
-    return false;
-  }
-
-  @Override
-  public String getLimitString(String sql, int offset, String offsetPlaceholder, int limit, String limitPlaceholder)
-  {
-    throw new UnsupportedOperationException("paged queries not supported");
-  }
+    @Override
+    public String getLimitString(String sql, int offset, String offsetPlaceholder, int limit, String limitPlaceholder) {
+        throw new UnsupportedOperationException("paged queries not supported");
+    }
 }
