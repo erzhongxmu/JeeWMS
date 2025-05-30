754268f1dc2bf03b8d3db16c00a54652a441d5e0
LongLiu <2074413691@qq.com>
2025-05-30 17:52:11 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/web/system/service/impl/DynamicDataSourceServiceImpl.java
+++ src/main/java/org/jeecgframework/web/system/service/impl/DynamicDataSourceServiceImpl.java
@@ -9,44 +9,51 @@ import org.jeecgframework.core.util.ResourceUtil;
 import org.jeecgframework.web.system.service.DynamicDataSourceServiceI;
 import org.jeecgframework.web.system.pojo.base.DynamicDataSourceEntity;
 
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("dynamicDataSourceService")
 @Transactional
 public class DynamicDataSourceServiceImpl extends CommonServiceImpl implements DynamicDataSourceServiceI {
 
-	/**初始化数据库信息，TOMCAT启动时直接加入到内存中**/
-	@Override
-	public List<DynamicDataSourceEntity> initDynamicDataSource() {
-		ResourceUtil.dynamicDataSourceMap.clear();
+    /**
+     * 初始化数据库信息，TOMCAT启动时直接加入到内存中
+     **/
+    @Override
+    public List<DynamicDataSourceEntity> initDynamicDataSource() {
+        ResourceUtil.dynamicDataSourceMap.clear();
 
-		List<DynamicDataSourceEntity> dynamicSourceEntityList = this.commonDao.loadAll(DynamicDataSourceEntity.class);
+        List<DynamicDataSourceEntity> dynamicSourceEntityList = this.commonDao.loadAll(DynamicDataSourceEntity.class);
 
-		for (DynamicDataSourceEntity dynamicSourceEntity : dynamicSourceEntityList) {
-			ResourceUtil.dynamicDataSourceMap.put(dynamicSourceEntity.getDbKey(), dynamicSourceEntity);
-		}
-		return dynamicSourceEntityList;
-	}
+        for (DynamicDataSourceEntity dynamicSourceEntity : dynamicSourceEntityList) {
+            ResourceUtil.dynamicDataSourceMap.put(dynamicSourceEntity.getDbKey(), dynamicSourceEntity);
+        }
+        return dynamicSourceEntityList;
+    }
 
-	public static DynamicDataSourceEntity getDbSourceEntityByKey(String dbKey) {
-		DynamicDataSourceEntity dynamicDataSourceEntity = ResourceUtil.dynamicDataSourceMap.get(dbKey);
+    public static DynamicDataSourceEntity getDbSourceEntityByKey(String dbKey) {
+        DynamicDataSourceEntity dynamicDataSourceEntity = ResourceUtil.dynamicDataSourceMap.get(dbKey);
 
-		return dynamicDataSourceEntity;
-	}
+        return dynamicDataSourceEntity;
+    }
 
-	@Override
-	public void refleshCache() {
-		initDynamicDataSource();
-	}
+    @Override
+    public void refleshCache() {
+        initDynamicDataSource();
+    }
 
-	@Override
+    @Override
 
-	public DynamicDataSourceEntity getDynamicDataSourceEntityForDbKey(String dbKey){
-		List<DynamicDataSourceEntity> dynamicDataSourceEntitys = commonDao.findHql("from DynamicDataSourceEntity where dbKey = ?", dbKey);
-		if(dynamicDataSourceEntitys.size()>0) {
+    public DynamicDataSourceEntity getDynamicDataSourceEntityForDbKey(String dbKey) {
+        List<DynamicDataSourceEntity> dynamicDataSourceEntitys = commonDao.findHql("from DynamicDataSourceEntity where dbKey = ?", dbKey);
+        if (dynamicDataSourceEntitys.size() > 0) {
             return dynamicDataSourceEntitys.get(0);
         }
-		return null;
-	}
+        return null;
+    }
 
 
 }
