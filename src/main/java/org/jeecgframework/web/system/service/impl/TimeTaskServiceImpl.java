6ecaa46e0b37975f76a5365aeef4046c8eaf1bfc
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-05-30 14:19:23 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/web/system/service/impl/TimeTaskServiceImpl.java
+++ src/main/java/org/jeecgframework/web/system/service/impl/TimeTaskServiceImpl.java
@@ -6,9 +6,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.jeecgframework.web.system.service.TimeTaskServiceI;
 
 import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("timeTaskService")
 @Transactional
 public class TimeTaskServiceImpl extends CommonServiceImpl implements TimeTaskServiceI {
-	
+
 }
