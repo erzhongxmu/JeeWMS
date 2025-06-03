84fefa1290e1e9bc8ad44378b16641302d878cc2
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-06-03 09:57:57 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/conf/service/WxConfigServiceI.java
+++ src/main/java/com/zzjee/conf/service/WxConfigServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.conf.entity.WxConfigEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WxConfigServiceI extends CommonService{
-	
+
  	public void delete(WxConfigEntity entity) throws Exception;
- 	
+
  	public Serializable save(WxConfigEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WxConfigEntity entity) throws Exception;
- 	
+
 }
