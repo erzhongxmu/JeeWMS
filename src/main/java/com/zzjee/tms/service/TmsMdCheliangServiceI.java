f46ba1670947c35487536e277ad67abb7158ae55
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-06-03 16:38:59 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/tms/service/TmsMdCheliangServiceI.java
+++ src/main/java/com/zzjee/tms/service/TmsMdCheliangServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.tms.entity.TmsMdCheliangEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface TmsMdCheliangServiceI extends CommonService {
-	
+
  	public void delete(TmsMdCheliangEntity entity) throws Exception;
- 	
+
  	public Serializable save(TmsMdCheliangEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(TmsMdCheliangEntity entity) throws Exception;
- 	
+
 }
