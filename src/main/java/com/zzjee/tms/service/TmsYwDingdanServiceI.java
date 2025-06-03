146bf2fc6be0d28808fc678dcb24211e0bd5cdef
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-06-03 16:39:39 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/tms/service/TmsYwDingdanServiceI.java
+++ src/main/java/com/zzjee/tms/service/TmsYwDingdanServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.tms.entity.TmsYwDingdanEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface TmsYwDingdanServiceI extends CommonService {
-	
+
  	public void delete(TmsYwDingdanEntity entity) throws Exception;
- 	
+
  	public Serializable save(TmsYwDingdanEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(TmsYwDingdanEntity entity) throws Exception;
- 	
+
 }
