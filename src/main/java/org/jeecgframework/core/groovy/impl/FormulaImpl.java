946321c738703536bea84f88edcbc2fa5fdd6e3b
LongLiu <2074413691@qq.com>
2025-06-05 18:45:42 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/core/groovy/impl/FormulaImpl.java
+++ src/main/java/org/jeecgframework/core/groovy/impl/FormulaImpl.java
@@ -6,9 +6,16 @@ import org.springframework.stereotype.Component;
 /**
  * 公式脚本
  */
+
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Component
 public class FormulaImpl implements IScript {
-	public Double add(Double a,Double b) {
-		return a*b;
-	}
+    public Double add(Double a, Double b) {
+        return a * b;
+    }
 }
