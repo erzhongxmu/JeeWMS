29b53a8c6ece04ea4ef8c2f7a32fc3cf2223aa30
LongLiu <2074413691@qq.com>
2025-06-05 18:45:17 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/core/util/ExceptionUtils.java
+++ src/main/java/org/jeecgframework/core/util/ExceptionUtils.java
@@ -2,38 +2,45 @@ package org.jeecgframework.core.util;
 
 import org.jeecgframework.core.common.exception.BusinessException;
 
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public class ExceptionUtils {
 
-	private ExceptionUtils(){
-		//no instance
-	}
-	
-	
-	/**
-	 * 如果目标为空则抛出异常
-	 * @author 苍鹰
-	 * 2009-11-3
-	 * @param target
-	 * @param errorMessage
-	 */
-	public static void throwIfNull(Object target,String errorMessage){
-		if(target==null){
-			throw new BusinessException(errorMessage);
-		}
-	}
-	
-	/**
-	 * 如果目标为空则抛出异常
-	 * 本方法空指针安全
-	 * 2009-11-3
-	 * @param target
-	 * @param errorMessage
-	 */
-	public static void throwIfEmpty(String target,String errorMessage)
-	{
-		if(target==null || "".equals(target)){
-			throw new BusinessException(errorMessage);
-		}
-	}
-	
+    private ExceptionUtils() {
+        //no instance
+    }
+
+
+    /**
+     * 如果目标为空则抛出异常
+     *
+     * @param target
+     * @param errorMessage
+     * @author 苍鹰
+     * 2009-11-3
+     */
+    public static void throwIfNull(Object target, String errorMessage) {
+        if (target == null) {
+            throw new BusinessException(errorMessage);
+        }
+    }
+
+    /**
+     * 如果目标为空则抛出异常
+     * 本方法空指针安全
+     * 2009-11-3
+     *
+     * @param target
+     * @param errorMessage
+     */
+    public static void throwIfEmpty(String target, String errorMessage) {
+        if (target == null || "".equals(target)) {
+            throw new BusinessException(errorMessage);
+        }
+    }
+
 }
