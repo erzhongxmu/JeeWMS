c2f06d8174977ffc3eaa9f70746f8f6879d2c1f0
LongLiu <2074413691@qq.com>
2025-06-06 18:47:13 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/core/util/ListtoJson.java
+++ src/main/java/org/jeecgframework/core/util/ListtoJson.java
@@ -5,57 +5,60 @@ import java.util.List;
 import org.jeecgframework.web.system.pojo.base.TSFunction;
 
 
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public class ListtoJson {
 
-	public static String getJsonData(@SuppressWarnings("rawtypes") List list) {
+    public static String getJsonData(@SuppressWarnings("rawtypes") List list) {
 
-		StringBuffer buffer = new StringBuffer();
+        StringBuffer buffer = new StringBuffer();
 
-		buffer.append("[");
+        buffer.append("[");
 
-		iterGet(list, "0", buffer);
+        iterGet(list, "0", buffer);
 
-		buffer.append("]");
+        buffer.append("]");
 
-		// 将,\n]替换成\n]
+        // 将,\n]替换成\n]
 
-		String tmp = buffer.toString();
+        String tmp = buffer.toString();
 
-		tmp = tmp.replaceAll(",\n]", "\n]");
+        tmp = tmp.replaceAll(",\n]", "\n]");
 
-		return tmp;
+        return tmp;
 
-	}
+    }
 
-	static int count = 0;
+    static int count = 0;
 
-	/**
-	 * 
-	 * 递归生成json格式的数据{id:1,text:'',children:[]}
-	 * 
-	 * @param args
-	 */
+    /**
+     * 递归生成json格式的数据{id:1,text:'',children:[]}
+     *
+     * @param args
+     */
 
-	static void iterGet(List<TSFunction> list, String pid, StringBuffer buffer) {
-		for (TSFunction node : list) {
+    static void iterGet(List<TSFunction> list, String pid, StringBuffer buffer) {
+        for (TSFunction node : list) {
 
-			// 查找所有父节点为pid的所有对象，然后拼接为json格式的数据
-			if(node.getTSFunction()!=null)
-			{			
-			if (pid.equals(oConvertUtils.getString(node.getTSFunction().getId())))
-			{
-				count++;
-				buffer.append("{\'id\':" + node.getId() + ",\'text\':\'"
-						+ node.getFunctionName() + "\',\'children\':[");
-				// 递归
-				iterGet(list, node.getId(), buffer);
-				buffer.append("]},\n");
-				count--;
+            // 查找所有父节点为pid的所有对象，然后拼接为json格式的数据
+            if (node.getTSFunction() != null) {
+                if (pid.equals(oConvertUtils.getString(node.getTSFunction().getId()))) {
+                    count++;
+                    buffer.append("{\'id\':" + node.getId() + ",\'text\':\'"
+                            + node.getFunctionName() + "\',\'children\':[");
+                    // 递归
+                    iterGet(list, node.getId(), buffer);
+                    buffer.append("]},\n");
+                    count--;
 
-			}
-			}
-		}
+                }
+            }
+        }
 
-	}
+    }
 
 }
