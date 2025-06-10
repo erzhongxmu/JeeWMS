db801fcf87fd01547d0ccf36cde0a46943d65394
LongLiu <2074413691@qq.com>
2025-06-10 18:03:10 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/web/system/sms/service/TSSmsTemplateServiceI.java
+++ src/main/java/org/jeecgframework/web/system/sms/service/TSSmsTemplateServiceI.java
@@ -1,36 +1,48 @@
 package org.jeecgframework.web.system.sms.service;
+
 import org.jeecgframework.web.system.sms.entity.TSSmsTemplateEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
 
-public interface TSSmsTemplateServiceI extends CommonService{
-	
- 	@Override
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
+public interface TSSmsTemplateServiceI extends CommonService {
+
+    @Override
     public <T> void delete(T entity);
- 	
- 	@Override
+
+    @Override
     public <T> Serializable save(T entity);
- 	
- 	@Override
+
+    @Override
     public <T> void saveOrUpdate(T entity);
- 	
- 	/**
-	 * 默认按钮-sql增强-新增操作
-	 * @param t
-	 * @return
-	 */
- 	public boolean doAddSql(TSSmsTemplateEntity t);
- 	/**
-	 * 默认按钮-sql增强-更新操作
-	 * @param t
-	 * @return
-	 */
- 	public boolean doUpdateSql(TSSmsTemplateEntity t);
- 	/**
-	 * 默认按钮-sql增强-删除操作
-	 * @param t
-	 * @return
-	 */
- 	public boolean doDelSql(TSSmsTemplateEntity t);
+
+    /**
+     * 默认按钮-sql增强-新增操作
+     *
+     * @param t
+     * @return
+     */
+    public boolean doAddSql(TSSmsTemplateEntity t);
+
+    /**
+     * 默认按钮-sql增强-更新操作
+     *
+     * @param t
+     * @return
+     */
+    public boolean doUpdateSql(TSSmsTemplateEntity t);
+
+    /**
+     * 默认按钮-sql增强-删除操作
+     *
+     * @param t
+     * @return
+     */
+    public boolean doDelSql(TSSmsTemplateEntity t);
 }
