49187e7954914d06b4ed9212ec0026d05170b72a
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-09 13:44:42 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/org/jeecgframework/web/system/service/TimeTaskServiceI.java
+++ src/main/java/org/jeecgframework/web/system/service/TimeTaskServiceI.java
@@ -1,7 +1,12 @@
 package org.jeecgframework.web.system.service;
 
 import org.jeecgframework.core.common.service.CommonService;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface TimeTaskServiceI extends CommonService{
 
 }
