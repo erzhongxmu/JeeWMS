8f99f6b800df2d9e3b4fd79ebb29ec07378ef5ec
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-11 10:17:53 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/bm/service/MvCusCostServiceI.java
+++ src/main/java/com/zzjee/bm/service/MvCusCostServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.bm.entity.MvCusCostEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface MvCusCostServiceI extends CommonService{
-	
+
  	public void delete(MvCusCostEntity entity) throws Exception;
- 	
+
  	public Serializable save(MvCusCostEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(MvCusCostEntity entity) throws Exception;
- 	
+
 }
