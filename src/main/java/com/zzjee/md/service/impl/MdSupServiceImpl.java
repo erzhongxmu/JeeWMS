d3e0645b0aee5cf441b7a6326596ddabea1a7621
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-14 11:12:10 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/md/service/impl/MdSupServiceImpl.java
+++ src/main/java/com/zzjee/md/service/impl/MdSupServiceImpl.java
@@ -12,7 +12,12 @@ import org.jeecgframework.core.util.ApplicationContextUtil;
 import org.jeecgframework.core.util.MyClassLoader;
 import org.jeecgframework.core.util.StringUtil;
 import org.jeecgframework.web.cgform.enhance.CgformEnhanceJavaInter;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("mdSupService")
 @Transactional
 public class MdSupServiceImpl extends CommonServiceImpl implements MdSupServiceI {
