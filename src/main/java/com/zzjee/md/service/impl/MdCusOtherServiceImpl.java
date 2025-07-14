e1ad75164a3d5e5d23fe1b424caf7dfeb09aa0c3
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-14 11:10:34 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/md/service/impl/MdCusOtherServiceImpl.java
+++ src/main/java/com/zzjee/md/service/impl/MdCusOtherServiceImpl.java
@@ -12,7 +12,12 @@ import org.jeecgframework.core.util.ApplicationContextUtil;
 import org.jeecgframework.core.util.MyClassLoader;
 import org.jeecgframework.core.util.StringUtil;
 import org.jeecgframework.web.cgform.enhance.CgformEnhanceJavaInter;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("mdCusOtherService")
 @Transactional
 public class MdCusOtherServiceImpl extends CommonServiceImpl implements MdCusOtherServiceI {
