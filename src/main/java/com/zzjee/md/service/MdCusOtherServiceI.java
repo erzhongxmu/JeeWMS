1704994ea8c8eab1c0e48822e0895455e9492088
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-15 17:25:48 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/md/service/MdCusOtherServiceI.java
+++ src/main/java/com/zzjee/md/service/MdCusOtherServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.md.entity.MdCusOtherEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface MdCusOtherServiceI extends CommonService{
-	
+
  	public void delete(MdCusOtherEntity entity) throws Exception;
- 	
+
  	public Serializable save(MdCusOtherEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(MdCusOtherEntity entity) throws Exception;
- 	
+
 }
