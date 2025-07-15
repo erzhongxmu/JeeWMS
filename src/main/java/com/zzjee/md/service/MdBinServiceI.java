268f6f79d2d8864ef395e5133ebfba23beee1f79
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-15 17:24:58 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/md/service/MdBinServiceI.java
+++ src/main/java/com/zzjee/md/service/MdBinServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.md.entity.MdBinEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface MdBinServiceI extends CommonService{
-	
+
  	public void delete(MdBinEntity entity) throws Exception;
- 	
+
  	public Serializable save(MdBinEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(MdBinEntity entity) throws Exception;
- 	
+
 }
