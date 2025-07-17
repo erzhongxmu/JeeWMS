4ba2e1a5eaf17296399a06e98524bf7f6b718e34
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-17 10:19:22 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/mvyj/service/MvStockYjServiceI.java
+++ src/main/java/com/zzjee/mvyj/service/MvStockYjServiceI.java
@@ -7,6 +7,12 @@ import java.io.Serializable;
  * MvStockYjServiceI 接口定义了对 MvStockYjEntity 实体类的增删改查操作，扩展了 CommonService 接口。
  * 方法中使用了 Exception 类型的异常，以应对操作中可能出现的问题。
  */
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface MvStockYjServiceI extends CommonService{
 	//根据实体类对象删除记录；
  	public void delete(MvStockYjEntity entity) throws Exception;
@@ -14,5 +20,5 @@ public interface MvStockYjServiceI extends CommonService{
  	public Serializable save(MvStockYjEntity entity) throws Exception;
  	//如果实体类对象已存在，更新其记录；否则，保存一条新记录。
  	public void saveOrUpdate(MvStockYjEntity entity) throws Exception;
- 	
+
 }
