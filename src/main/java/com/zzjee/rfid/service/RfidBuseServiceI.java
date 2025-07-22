5123f3ed9cfd4d6243121a8c6aa98752e9d5eb7b
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-22 15:14:56 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/rfid/service/RfidBuseServiceI.java
+++ src/main/java/com/zzjee/rfid/service/RfidBuseServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.rfid.entity.RfidBuseEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface RfidBuseServiceI extends CommonService{
-	
+
  	public void delete(RfidBuseEntity entity) throws Exception;
- 	
+
  	public Serializable save(RfidBuseEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(RfidBuseEntity entity) throws Exception;
- 	
+
 }
