e1bb2bfee0b6aeca108b5ca6fae2d2c0c6a8929a
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-23 15:56:52 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/sys/service/SysParaServiceI.java
+++ src/main/java/com/zzjee/sys/service/SysParaServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.sys.entity.SysParaEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface SysParaServiceI extends CommonService{
-	
+
  	public void delete(SysParaEntity entity) throws Exception;
- 	
+
  	public Serializable save(SysParaEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(SysParaEntity entity) throws Exception;
- 	
+
 }
