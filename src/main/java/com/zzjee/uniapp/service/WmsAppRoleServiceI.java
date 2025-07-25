83d5439c7db413bd26e86323e6123d1611ab2f8f
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-25 16:50:08 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/uniapp/service/WmsAppRoleServiceI.java
+++ src/main/java/com/zzjee/uniapp/service/WmsAppRoleServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.uniapp.entity.WmsAppRoleEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmsAppRoleServiceI extends CommonService{
-	
+
  	public void delete(WmsAppRoleEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmsAppRoleEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmsAppRoleEntity entity) throws Exception;
- 	
+
 }
