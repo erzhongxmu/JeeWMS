79197db480308439455ee909c13d188a35017155
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-25 16:49:32 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/uniapp/service/WmsAppFunctionServiceI.java
+++ src/main/java/com/zzjee/uniapp/service/WmsAppFunctionServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.uniapp.entity.WmsAppFunctionEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmsAppFunctionServiceI extends CommonService{
-	
+
  	public void delete(WmsAppFunctionEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmsAppFunctionEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmsAppFunctionEntity entity) throws Exception;
- 	
+
 }
