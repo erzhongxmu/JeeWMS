5d244797f194f17260be631f3341be3a5f96ddd8
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-29 15:46:52 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wave/service/WaveToDownServiceI.java
+++ src/main/java/com/zzjee/wave/service/WaveToDownServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wave.entity.WaveToDownEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WaveToDownServiceI extends CommonService{
-	
+
  	public void delete(WaveToDownEntity entity) throws Exception;
- 	
+
  	public Serializable save(WaveToDownEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WaveToDownEntity entity) throws Exception;
- 	
+
 }
