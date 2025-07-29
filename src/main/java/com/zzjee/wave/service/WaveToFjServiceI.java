416a20769e9085b28da96220e571df2f5aeb96df
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-29 15:47:15 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wave/service/WaveToFjServiceI.java
+++ src/main/java/com/zzjee/wave/service/WaveToFjServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wave.entity.WaveToFjEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WaveToFjServiceI extends CommonService{
-	
+
  	public void delete(WaveToFjEntity entity) throws Exception;
- 	
+
  	public Serializable save(WaveToFjEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WaveToFjEntity entity) throws Exception;
- 	
+
 }
