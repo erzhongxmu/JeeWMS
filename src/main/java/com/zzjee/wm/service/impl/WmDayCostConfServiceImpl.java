65894fb012689f6d96208436e7b9d96c9f4b5ede
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-07-31 15:12:36 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/impl/WmDayCostConfServiceImpl.java
+++ src/main/java/com/zzjee/wm/service/impl/WmDayCostConfServiceImpl.java
@@ -12,19 +12,24 @@ import org.jeecgframework.core.util.ApplicationContextUtil;
 import org.jeecgframework.core.util.MyClassLoader;
 import org.jeecgframework.core.util.StringUtil;
 import org.jeecgframework.web.cgform.enhance.CgformEnhanceJavaInter;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 @Service("wmDayCostConfService")
 @Transactional
 public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDayCostConfServiceI {
 
-	
+
  	@Override
     public void delete(WmDayCostConfEntity entity) throws Exception{
  		super.delete(entity);
  		//执行删除操作增强业务
 		this.doDelBus(entity);
  	}
- 	
+
  	@Override
     public Serializable save(WmDayCostConfEntity entity) throws Exception{
  		Serializable t = super.save(entity);
@@ -32,14 +37,14 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
  		this.doAddBus(entity);
  		return t;
  	}
- 	
+
  	@Override
     public void saveOrUpdate(WmDayCostConfEntity entity) throws Exception{
  		super.saveOrUpdate(entity);
  		//执行更新操作增强业务
  		this.doUpdateBus(entity);
  	}
- 	
+
  	/**
 	 * 新增操作增强业务
 	 * @param t
@@ -48,7 +53,7 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
 	private void doAddBus(WmDayCostConfEntity t) throws Exception{
 		//-----------------sql增强 start----------------------------
 	 	//-----------------sql增强 end------------------------------
-	 	
+
 	 	//-----------------java增强 start---------------------------
 	 	//-----------------java增强 end-----------------------------
  	}
@@ -60,7 +65,7 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
 	private void doUpdateBus(WmDayCostConfEntity t) throws Exception{
 		//-----------------sql增强 start----------------------------
 	 	//-----------------sql增强 end------------------------------
-	 	
+
 	 	//-----------------java增强 start---------------------------
 	 	//-----------------java增强 end-----------------------------
  	}
@@ -72,11 +77,11 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
 	private void doDelBus(WmDayCostConfEntity t) throws Exception{
 	    //-----------------sql增强 start----------------------------
 	 	//-----------------sql增强 end------------------------------
-	 	
+
 	 	//-----------------java增强 start---------------------------
 	 	//-----------------java增强 end-----------------------------
  	}
- 	
+
  	private Map<String,Object> populationMap(WmDayCostConfEntity t){
 		Map<String,Object> map = new HashMap<String,Object>(1024);
 		map.put("id", t.getId());
@@ -92,7 +97,7 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
 		map.put("cost_sf", t.getCostSf());
 		return map;
 	}
- 	
+
  	/**
 	 * 替换sql中的变量
 	 * @param sql
@@ -114,7 +119,7 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
  		sql  = sql.replace("#{UUID}",UUID.randomUUID().toString());
  		return sql;
  	}
- 	
+
  	/**
 	 * 执行JAVA增强
 	 */
@@ -135,7 +140,7 @@ public class WmDayCostConfServiceImpl extends CommonServiceImpl implements WmDay
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new Exception("执行JAVA增强出现异常！");
-			} 
+			}
 		}
  	}
 }
