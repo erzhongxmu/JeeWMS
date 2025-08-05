fc1a6f4bd67f821390a56b9ea6cbda1199bc6b58
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-05 16:58:35 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/WmDayCostConfServiceI.java
+++ src/main/java/com/zzjee/wm/service/WmDayCostConfServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wm.entity.WmDayCostConfEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmDayCostConfServiceI extends CommonService{
-	
+
  	public void delete(WmDayCostConfEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmDayCostConfEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmDayCostConfEntity entity) throws Exception;
- 	
+
 }
