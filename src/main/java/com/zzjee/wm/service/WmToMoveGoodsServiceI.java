343934270a3c55b80ed31fbc5624f545d3637982
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-06 11:34:48 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/WmToMoveGoodsServiceI.java
+++ src/main/java/com/zzjee/wm/service/WmToMoveGoodsServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wm.entity.WmToMoveGoodsEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmToMoveGoodsServiceI extends CommonService{
-	
+
  	public void delete(WmToMoveGoodsEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmToMoveGoodsEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmToMoveGoodsEntity entity) throws Exception;
- 	
+
 }
