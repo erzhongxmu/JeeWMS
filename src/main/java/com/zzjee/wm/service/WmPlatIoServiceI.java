9b102784157ac46741cee2951b511a5bd0eba1f9
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-06 11:32:19 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/WmPlatIoServiceI.java
+++ src/main/java/com/zzjee/wm/service/WmPlatIoServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wm.entity.WmPlatIoEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmPlatIoServiceI extends CommonService{
-	
+
  	public void delete(WmPlatIoEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmPlatIoEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmPlatIoEntity entity) throws Exception;
- 	
+
 }
