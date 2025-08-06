ed3c0309e1d159b826dc995b7410bb4ba267c1b6
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-06 11:33:52 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/WmToDownGoodsServiceI.java
+++ src/main/java/com/zzjee/wm/service/WmToDownGoodsServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wm.entity.WmToDownGoodsEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WmToDownGoodsServiceI extends CommonService{
-	
+
  	public void delete(WmToDownGoodsEntity entity) throws Exception;
- 	
+
  	public Serializable save(WmToDownGoodsEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WmToDownGoodsEntity entity) throws Exception;
- 	
+
 }
