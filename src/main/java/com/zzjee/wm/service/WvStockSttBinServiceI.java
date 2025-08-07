9f42e601adcedbcfb8e4d1d9599a37e9f2c5c783
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-07 14:04:58 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wm/service/WvStockSttBinServiceI.java
+++ src/main/java/com/zzjee/wm/service/WvStockSttBinServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wm.entity.WvStockSttBinEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WvStockSttBinServiceI extends CommonService{
-	
+
  	public void delete(WvStockSttBinEntity entity) throws Exception;
- 	
+
  	public Serializable save(WvStockSttBinEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WvStockSttBinEntity entity) throws Exception;
- 	
+
 }
