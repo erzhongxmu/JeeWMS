c347b3f49639cbe0e2cd90f6750cba51c020baa1
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-11 17:45:48 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wmapi/service/WvGiServiceI.java
+++ src/main/java/com/zzjee/wmapi/service/WvGiServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wmapi.entity.WvGiEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface WvGiServiceI extends CommonService{
-	
+
  	public void delete(WvGiEntity entity) throws Exception;
- 	
+
  	public Serializable save(WvGiEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(WvGiEntity entity) throws Exception;
- 	
+
 }
