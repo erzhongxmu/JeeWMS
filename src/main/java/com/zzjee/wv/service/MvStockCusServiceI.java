f7f75fc01abef269af8173fec37a33ab2db77913
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-08-13 15:01:28 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/wv/service/MvStockCusServiceI.java
+++ src/main/java/com/zzjee/wv/service/MvStockCusServiceI.java
@@ -3,13 +3,18 @@ import com.zzjee.wv.entity.MvStockCusEntity;
 import org.jeecgframework.core.common.service.CommonService;
 
 import java.io.Serializable;
-
+/**
+ * Demo class
+ *
+ * @author admin
+ * @date 2016/10/31
+ */
 public interface MvStockCusServiceI extends CommonService{
-	
+
  	public void delete(MvStockCusEntity entity) throws Exception;
- 	
+
  	public Serializable save(MvStockCusEntity entity) throws Exception;
- 	
+
  	public void saveOrUpdate(MvStockCusEntity entity) throws Exception;
- 	
+
 }
