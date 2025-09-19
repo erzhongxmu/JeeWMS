b7cb0d5caad4ce717bf5e3fbb30dc16bc6530d66
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-09-19 16:55:51 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/ba/controller/BaUnitController.java
+++ src/main/java/com/zzjee/ba/controller/BaUnitController.java
@@ -414,7 +414,10 @@ public class BaUnitController extends BaseController {
 		//按Restful约定，返回204状态码, 无内容. 也可以返回200状态码.
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
-
+	/**
+	 * 删除计量单位
+	 * @param id
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") String id) {
