d9a299efcf2afb8f31530ae81f12963693c9bbb9
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-10-17 17:44:25 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/conf/controller/FxjOtherLoginController.java
+++ src/main/java/com/zzjee/conf/controller/FxjOtherLoginController.java
@@ -360,7 +360,12 @@ public class FxjOtherLoginController extends BaseController {
 		}
 		return new ResponseEntity(task, HttpStatus.OK);
 	}
-
+	/**
+	 * 创建第三方登录
+	 * @param fxjOtherLogin
+	 * @param uriBuilder
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<?> create(@RequestBody FxjOtherLoginEntity fxjOtherLogin, UriComponentsBuilder uriBuilder) {
