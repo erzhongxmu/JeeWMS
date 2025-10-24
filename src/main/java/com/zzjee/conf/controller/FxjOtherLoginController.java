93a16371833647ffb2e806e2fa2ebb8d05355c08
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-10-24 11:18:23 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/conf/controller/FxjOtherLoginController.java
+++ src/main/java/com/zzjee/conf/controller/FxjOtherLoginController.java
@@ -410,7 +410,10 @@ public class FxjOtherLoginController extends BaseController {
 		//按Restful约定，返回204状态码, 无内容. 也可以返回200状态码.
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
-
+	/**
+	 * 删除第三方登录
+	 * @param id
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") String id) {
