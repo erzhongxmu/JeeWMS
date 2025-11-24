a5d5e2e57febb535c1205d45ecf1364ea9f997f4
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2025-11-24 17:20:34 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/md/controller/MdBinController.java
+++ src/main/java/com/zzjee/md/controller/MdBinController.java
@@ -713,7 +713,11 @@ public class MdBinController extends BaseController {
         headers.setLocation(uri);
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
-
+    /**
+     * 更新仓位定义
+     * @param mdBin
+     * @return
+     */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> update(@RequestBody MdBinEntity mdBin) {
         //调用JSR303 Bean Validator进行校验，如果出错返回含400错误码及json格式的错误信息.
