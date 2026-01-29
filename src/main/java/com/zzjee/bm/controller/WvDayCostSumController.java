4df843d0cb19b39c46df62c2e3270f389d727d2a
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2026-01-29 10:06:01 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/bm/controller/WvDayCostSumController.java
+++ src/main/java/com/zzjee/bm/controller/WvDayCostSumController.java
@@ -460,7 +460,12 @@ public class WvDayCostSumController extends BaseController {
 		}
 		return new ResponseEntity(task, HttpStatus.OK);
 	}
-
+	/**
+	 * 创建WvDayCostSumEntity列表
+	 * @param wvDayCostSum
+	 * @param uriBuilder
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<?> create(@RequestBody WvDayCostSumEntity wvDayCostSum, UriComponentsBuilder uriBuilder) {
