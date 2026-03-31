7bd40d8f0880dfe6298e68f7257acb475429b07d
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2026-03-31 15:02:37 +0800

    阿里巴巴代码规范，所有的类都必须添加创建者信息

--- src/main/java/com/zzjee/mvyj/controller/MvStockYjController.java
+++ src/main/java/com/zzjee/mvyj/controller/MvStockYjController.java
@@ -538,7 +538,10 @@ public class MvStockYjController extends BaseController {
 		//按Restful约定，返回204状态码, 无内容. 也可以返回200状态码.
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
-
+	/**
+	 * 删除效期预警
+	 * @param id
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") String id) {
