9559488c6c2840cd37d4cb60e004da38ddc18128
蔡蔡蔡 <14082214+yangming0707@user.noreply.gitee.com>
2026-08-20 17:11:28 +0800

    diccode 字典编码

--- src/main/java/org/jeecgframework/web/cgdynamgraph/controller/CgDynamGraphController.java
+++ src/main/java/org/jeecgframework/web/cgdynamgraph/controller/CgDynamGraphController.java
@@ -418,12 +418,10 @@ public class CgDynamGraphController extends BaseController {
 	 * @return
 	 */
 	private List<Map<String, Object>> queryDic(String diccode) {
-		StringBuilder dicSql = new StringBuilder();
-		dicSql.append(" SELECT TYPECODE,TYPENAME FROM");
-		dicSql.append(" "+CgReportConstant.SYS_DIC);
-		dicSql.append(" "+"WHERE TYPEGROUPID = ");
-		dicSql.append(" "+"(SELECT ID FROM "+CgReportConstant.SYS_DICGROUP+" WHERE TYPEGROUPCODE = '"+diccode+"' )");
-		List<Map<String, Object>> dicDatas = cgDynamGraphService.findForJdbc(dicSql.toString());
+		String dicSql = "SELECT TYPECODE,TYPENAME FROM " + CgReportConstant.SYS_DIC
+				+ " WHERE TYPEGROUPID = (SELECT ID FROM " + CgReportConstant.SYS_DICGROUP
+				+ " WHERE TYPEGROUPCODE = ?)";
+		List<Map<String, Object>> dicDatas = cgDynamGraphService.findForJdbc(dicSql, diccode);
 		return dicDatas;
 	}
 }
